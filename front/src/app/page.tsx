80ae37ae15dbdab06389cfdc6e31e53c427419c7
Rousselle <ylan.rousselle76@gmail.com>
2025-01-13 17:47:54 +0100

    formatage

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -28,8 +28,8 @@ export interface ApiResponse {
 
 export default function Home() {
   const [inputValue, setInputValue] = useState(""); // Pour stocker la valeur de l'input
-  const [apiResponse, setApiResponse] = useState<null|ApiResponse>(); // Pour stocker la réponse de l'API
-  const [responseError, setResponseError] = useState<null|String>(); // Pour stocker la réponse de l'API
+  const [apiResponse, setApiResponse] = useState<null | ApiResponse>(); // Pour stocker la réponse de l'API
+  const [responseError, setResponseError] = useState<null | string>(); // Pour stocker la réponse de l'API
   const [loading, setLoading] = useState(false); // Pour gérer l'état de chargement
 
   const handleFetchData = async () => {
@@ -48,17 +48,16 @@ export default function Home() {
         "shareUrl": inputValue
       });
 
-
       const response = await fetch("http://localhost:4000/", {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          redirect: "follow"
-        });
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+      });
 
       if (!response.ok) throw new Error("Erreur lors de l'appel API");
-      
-      const data: ApiResponse= await response.json();
+
+      const data: ApiResponse = await response.json();
       console.log(data)
       setApiResponse(data); // Supposons que l'API retourne un champ `result`
       setResponseError(null)
@@ -90,17 +89,17 @@ export default function Home() {
         {responseError ?? (
           <p>{responseError}</p>
         )}
-        
+
         {apiResponse ? (
           <>
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-              {apiResponse.result.invest.companyName} 
+              {apiResponse.result.invest.companyName}
               <a href={apiResponse.url} className="text-3xl"> 🌐</a>
             </h1>
             <p className="mb-5 text-justify indent-2.5">{apiResponse.result.summary}</p>
             <Separator />
             <h2 className="text-3xl font-semibold text-gray-800 mt-4">
-              Bon investissement : {apiResponse.result.invest.goodToInvest ? ("Oui ✅"): ("non ❌")}
+              Bon investissement : {apiResponse.result.invest.goodToInvest ? ("Oui ✅") : ("non ❌")}
             </h2>
             <p className="text-sm text-muted-foreground">GPT à estimé un score de de {apiResponse.result.invest.craze}%</p>
             <h3 className="text-2xl font-medium text-gray-700 mt-3">Pourquoi investir ?</h3>
@@ -116,45 +115,45 @@ export default function Home() {
             <Separator />
             <h2 className="text-3xl font-semibold text-gray-800 mt-4">Liens utiles</h2>
             <ul>
-            {apiResponse.result.usefullLinks.map((link, index) => (
-              <li key={index}>
-                <a href={"https://www.zonebourse.com"+link.url} className="text-blue-500 hover:underline">
-                  {link.textUrl}
-                </a>
-              </li>
-            ))}
+              {apiResponse.result.usefullLinks.map((link, index) => (
+                <li key={index}>
+                  <a href={"https://www.zonebourse.com" + link.url} className="text-blue-500 hover:underline">
+                    {link.textUrl}
+                  </a>
+                </li>
+              ))}
             </ul>
           </>
         ) : (
           loading && !responseError ? (
             <div>
-            <Skeleton className="w-1/2 h-[40px] mb-5"/>
-            <Skeleton className="w-full h-[300px]"/>
-            <Separator />
-            <h2 className="text-3xl font-semibold text-gray-800 mt-4">
-              Bon investissement : <Skeleton className="w-1/4 h-[20px]" />
-            </h2>
-            <Skeleton className="text-sm text-muted-foreground" />
-            <h3 className="text-2xl font-medium text-gray-700 mt-3">Pourquoi investir ?</h3>
-            <Skeleton className="w-full h-[100px] " />
-            <h3 className="text-2xl font-medium text-gray-700 mt-3">Pourquoi ne pas investir ?</h3>
-            <Skeleton className="w-full h-[100px] mb-5" />
-            <Separator />
-            <h2 className="text-3xl font-semibold text-gray-800 mt-4">Données clés</h2>
-            <Skeleton className="w-full h-[500px]" />
-            <Separator />
-            <h2 className="text-3xl font-semibold text-gray-800 mt-4">Liens utiles</h2>
-            <Skeleton className="w-full h-[500px]" />
-          </div>
+              <Skeleton className="w-1/2 h-[40px] mb-5" />
+              <Skeleton className="w-full h-[300px]" />
+              <Separator />
+              <h2 className="text-3xl font-semibold text-gray-800 mt-4">
+                Bon investissement : <Skeleton className="w-1/4 h-[20px]" />
+              </h2>
+              <Skeleton className="text-sm text-muted-foreground" />
+              <h3 className="text-2xl font-medium text-gray-700 mt-3">Pourquoi investir ?</h3>
+              <Skeleton className="w-full h-[100px] " />
+              <h3 className="text-2xl font-medium text-gray-700 mt-3">Pourquoi ne pas investir ?</h3>
+              <Skeleton className="w-full h-[100px] mb-5" />
+              <Separator />
+              <h2 className="text-3xl font-semibold text-gray-800 mt-4">Données clés</h2>
+              <Skeleton className="w-full h-[500px]" />
+              <Separator />
+              <h2 className="text-3xl font-semibold text-gray-800 mt-4">Liens utiles</h2>
+              <Skeleton className="w-full h-[500px]" />
+            </div>
           ) : (
             !responseError ? (
               <div>
                 <p>Entrez une url</p>
               </div>
             ) : (<></>)
-            
+
           )
-          
+
         )}
       </ScrollArea>
     </div>
